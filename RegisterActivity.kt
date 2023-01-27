a61b6d5dcbd0d61276eb614ad2e9b6221a981c22
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-01-26 16:28:27 -0800

    Adding in spinner to activity

--- RegisterActivity.kt
+++ RegisterActivity.kt
@@ -1,1 +1,12 @@
-
+val spinner: Spinner = findViewById(R.id.spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout
+ArrayAdapter.createFromResource(
+        this,
+        R.array.account_type_array,
+        android.R.layout.simple_spinner_item
+).also { adapter ->
+    // Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    // Apply the adapter to the spinner
+    spinner.adapter = adapter
+}
