b491092a72cad04655cce235616d5c7bef3e14e2
theoyugenco <103534036+theoyugenco@users.noreply.github.com>
2023-02-01 14:31:11 -0800

    Merchant classes (User child class)

---
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/Merchant.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+class Merchant:User() {
+    //Planning to create an array of credit cards that the Merchant may use as their payment methods
+    //Going to find out if there are already classes/built-in library to use instead
+
+    //Planning to do the same with phone numbers
+
+    //Planning to do the same with locations
+}
