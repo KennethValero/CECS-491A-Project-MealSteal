5f2476dd4cb14944208dddd4582107a13faa1612
theoyugenco <103534036+theoyugenco@users.noreply.github.com>
2023-01-31 16:36:47 -0800

    User.kt changed (password + getters/setters) Customer.kt added (child class of User) unsure why gradle.xml and misc.xml updated Also first time committing and pushing

---
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/Customer.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+class Customer:User(){
+    //Planning to create an array of credit cards that the Customer may use as their payment methods
+    //Going to find out if there are already classes/built-in library to use instead
+
+    //Planning to do the same but for their addresses (that they could pull and use for both billing and delivery)
+
+
+}
--- MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/User.kt
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/User.kt
@@ -1,19 +1,41 @@
 package com.example.myapplication
 
-class User {
+//open lets the class be inheritable
+open class User {
     var name: String? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+
     var email: String? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var password: String? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    //There should be no way to change uid and account_type
     var uid: String? = null
     var account_type: String? = null
 
     constructor() {
     }
     
-    constructor(name: String?, email: String?, uid: String?, account_type: String?) {
+    constructor(name: String?, email: String?, password: String?, uid: String?, account_type: String?) {
         this.name = name
         this.email = email
+        this.password = password
         this.uid = uid
         this.account_type = account_type
     }
+
+
+
 }
 
