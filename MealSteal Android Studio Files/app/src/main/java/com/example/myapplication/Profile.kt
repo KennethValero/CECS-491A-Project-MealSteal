cc4ee69efe7f40923a872deb94cbecc74c98bf3e
theoyugenco <103534036+theoyugenco@users.noreply.github.com>
2023-02-02 16:03:00 -0800

    Minor Work on Profile and its layout.xml

--- MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/Profile.kt
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/Profile.kt
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 //If we are at this activity, then the User (either Merchant or Customer) has already successfully logged in.
 class Profile : AppCompatActivity() {
     private lateinit var binding::ActivityProfileBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,8 @@ class Profile : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         var currentUser = FirebaseAuth.getInstance().currentUser
+
+        database = FirebaseDatabase.getInstance().getReference()
 
 
     }
