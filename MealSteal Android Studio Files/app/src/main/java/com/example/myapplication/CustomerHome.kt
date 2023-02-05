3f5c34e9027ac88c742eef8c1c61b5f3b7e0cd83
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-02-04 16:43:07 -0800

    setting up buttons for chatroom

--- MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/CustomerHome.kt
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/CustomerHome.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CustomerHome : AppCompatActivity() {
+    private lateinit var btnChat: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customerhome)
+
+        btnChat = findViewById(R.id.btnCustChat)
+
+        btnChat.setOnClickListener()
+        {
+            startActivity(Intent(this, Register::class.java))
+        }
     }
 }
--- MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/MerchantHome.kt
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/MerchantHome.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MerchantHome : AppCompatActivity() {
+    private lateinit var btnChat: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_merchanthome)
+        btnChat = findViewById(R.id.btnMerChat)
+
+        btnChat.setOnClickListener()
+        {
+            startActivity(Intent(this, Register::class.java))
+        }
     }
 }
