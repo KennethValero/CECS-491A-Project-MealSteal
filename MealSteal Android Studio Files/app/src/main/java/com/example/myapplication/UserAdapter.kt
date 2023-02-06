c4ceed78700632d8b0cdaefdc86e3ec08bfd3857
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-02-05 20:49:38 -0800

    adding user layout and finishing adapter

--- MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/UserAdapter.kt
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/UserAdapter.kt
@@ -1,23 +1,29 @@
 package com.example.myapplication
+import android.content.Context
+import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
-class UserAdapter : RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+class UserAdapter (val context: Context, val userList: ArrayList<User>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val name = itemView.findViewById<TextView>(R.id.textUser)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(context).inflate(R.layout.user_layout, parent, false)
+        return UserViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return userList.size
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentUser = userList[position]
+
+        holder.name.text = currentUser.name
     }
 }
