6ea5e02cea6800280396bd9b4cc50141dde274c3
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-02-06 16:18:14 -0800

    layout xml for chat user search

---
+++ MealSteal Android Studio Files/app/src/main/java/com/example/myapplication/ChatSearch.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatSearch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_search)
+    }
+}
